3854b96d0733c2c24a734b33b812b748c227bbc4
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-05-17 06:32:13 +0300

    add conflicts

--- src/tasks/merge-turath/authors.ts
+++ src/tasks/merge-turath/authors.ts
@@ -4,42 +4,7 @@ import natural from 'natural';
 import path from 'path';
 import fs from 'fs';
 import { getAllBooks } from '@/datasources/turath/books';
-
-const OUTPUT_PATH = path.resolve('test/link-authors.json');
-const OUTPUT_PATH_BOOKS = path.resolve('test/link-books.json');
-
-function normalizeText(text: string) {
-  return (
-    text
-      .normalize('NFD')
-      // replace anything between parentheses or brackets
-      .replace(/[\(\[].*?[\)\]]/g, '')
-      .replace(/[\u0300-\u036f]/g, '')
-      .toLowerCase()
-      .trim()
-      .replace(/\s+/g, ' ')
-  );
-}
-
-const metaphone = new natural.Metaphone();
-const tokenizer = new natural.WordTokenizer();
-
-function arePhoneticallySimilar(a: string, b: string) {
-  return metaphone.compare(normalizeText(a), normalizeText(b));
-}
-
-function jaccardSimilarity(setA: Set<string>, setB: Set<string>) {
-  const intersection = new Set([...setA].filter(x => setB.has(x)));
-  const union = new Set([...setA, ...setB]);
-  return intersection.size / union.size;
-}
-
-function semanticSimilarity(a: string, b: string) {
-  const tokensA = new Set(tokenizer.tokenize(normalizeText(a)));
-  const tokensB = new Set(tokenizer.tokenize(normalizeText(b)));
-
-  return jaccardSimilarity(tokensA, tokensB);
-}
+import { jaroWinkler, normalizeText } from './utils';
 
 const openitiAuthors = await getAuthorsData({ populateBooks: true });
 const openitiAuthorIdToAuthor = openitiAuthors.reduce(
@@ -60,6 +25,109 @@ const turathAuthorIdToBooks = (await getAllBooks()).reduce(
   {} as Record<number, Awaited<ReturnType<typeof getAllBooks>>>,
 );
 
+// count turath authors with year
+console.log(
+  `${turathAuthors.filter(author => author.death).length} / ${turathAuthors.length} authors have death year.`,
+);
+
+const OUTPUT_PATH = path.resolve('test/link-authors.json');
+const CONFLICTS_OUTPUT_PATH = path.resolve('test/link-authors-conflicts.json');
+const OUTPUT_PATH_BOOKS = path.resolve('test/link-books.json');
+
+// const turathAuthor = turathAuthors.find(author => author.id === 7)!;
+// const openitiAuthor = openitiAuthors.find(author => author.slug === 'suyuti')!;
+
+// const tokenizer = new natural.WordTokenizer();
+// const metaphone = new natural.Metaphone();
+
+// function arePhoneticallySimilar(a: string, b: string) {
+//   return metaphone.compare(normalizeText(a), normalizeText(b));
+// }
+
+// function semanticSimilarity(a: string, b: string) {
+//   const tokensA = new Set(tokenizer.tokenize(normalizeText(a)));
+//   const tokensB = new Set(tokenizer.tokenize(normalizeText(b)));
+
+//   // jaccard similarity
+//   const intersection = new Set([...tokensA].filter(x => tokensB.has(x)));
+//   const union = new Set([...tokensA, ...tokensB]);
+//   return intersection.size / union.size;
+// }
+
+const THRESHOLD = 0.9;
+const doesNamesMatch = (a: string | string[], b: string | string[]): boolean => {
+  if (Array.isArray(a) || Array.isArray(b)) {
+    return (Array.isArray(a) ? a : [a]).some(name =>
+      (Array.isArray(b) ? b : [b]).some(name2 => doesNamesMatch(name, name2)),
+    );
+  }
+
+  return (
+    a === b ||
+    normalizeText(a) === normalizeText(b) ||
+    jaroWinkler(a, b) > THRESHOLD ||
+    jaroWinkler(normalizeText(a), normalizeText(b)) > THRESHOLD
+  );
+};
+
+const getOpenitiNames = (
+  author:
+    | (typeof openitiAuthors)[number]
+    | (typeof openitiAuthors)[number]['books'][number],
+) => {
+  return [
+    ...new Set(
+      author.primaryNames
+        .filter(name => name.locale === 'ar' || name.locale === 'fa')
+        .map(name => name.text)
+        .concat(
+          author.otherNames
+            .filter(name => name.locale === 'ar' || name.locale === 'fa')
+            .flatMap(name => name.texts),
+        ),
+    ),
+  ];
+};
+
+// console.log({
+//   openitiNames: getOpenitiNames(openitiAuthor),
+//   turathName: turathAuthor.name,
+//   normalized: {
+//     openiti: getOpenitiNames(openitiAuthor).map(normalizeText),
+//     turath: normalizeText(turathAuthor.name),
+//   },
+//   match: doesNamesMatch(getOpenitiNames(openitiAuthor), turathAuthor.name),
+//   scores: getOpenitiNames(openitiAuthor).map(name =>
+//     jaroWinkler(name, turathAuthor.name),
+//   ),
+//   normalizedScores: getOpenitiNames(openitiAuthor).map(name =>
+//     jaroWinkler(normalizeText(name), normalizeText(turathAuthor.name)),
+//   ),
+// });
+
+/**
+ *
+ * {
+  openitiNames: [
+    'جلال الدين السيوطي',
+    'جلال الدین سیوطی',
+    'جلال الدين، أبو الفضل عبد الرحمان بن أبي بكر بن محمد جلال الدين الخضيري الأسيوطي'
+  ],
+  turathName: 'الجلال السيوطي',
+  normalized: {
+    openiti: [
+      'جلال الدين السيوطي',
+      'جلال الدین سیوطی',
+      'جلال الدين، أبو الفضل عبد الرحمان بن أبي بكر بن محمد جلال الدين الخضيري الأسيوطي'
+    ],
+    turath: 'الجلال السيوطي'
+  },
+  match: false,
+  scores: [ 0.7830687830687829, 0.6964285714285715, 0.544047619047619 ],
+  normalizedScores: [ 0.7830687830687829, 0.6964285714285715, 0.544047619047619 ]
+}
+ */
+
 const deathYearToGroups: Record<
   number,
   {
@@ -72,14 +140,14 @@ for (const author of openitiAuthors) {
   let key = -1;
   if (author.year) key = author.year;
 
-  if (!deathYearToGroups[author.year]) {
-    deathYearToGroups[author.year] = {
+  if (!deathYearToGroups[key]) {
+    deathYearToGroups[key] = {
       openiti: [],
       turath: [],
     };
   }
 
-  deathYearToGroups[author.year]!.openiti.push(author);
+  deathYearToGroups[key]!.openiti.push(author);
 }
 
 for (const author of turathAuthors) {
@@ -98,6 +166,7 @@ for (const author of turathAuthors) {
 
 // openiti id -> turath id
 const result: Record<string, number> = {};
+const conflicts: Record<string, number> = {};
 const linkedAuthors = new Set<number>();
 
 let i = 1;
@@ -106,35 +175,35 @@ const total = Object.keys(deathYearToGroups).length;
 for (const year in deathYearToGroups) {
   console.log(`Processing year ${year} (${i} / ${total})`);
 
+  if (Number(year) === -1) continue;
+
   const data = deathYearToGroups[year]!;
 
-  let found = false;
-  for (const openitiAuthor of Number(year) === -1 ? openitiAuthors : data.openiti) {
+  const batchOpenitiAuthors =
+    Number(year) === -1 ? openitiAuthors.filter(({ id }) => !result[id]) : data.openiti;
+
+  for (const openitiAuthor of batchOpenitiAuthors) {
+    let found = false;
+
     for (const turathAuthor of data.turath) {
-      const openitiNames = openitiAuthor.primaryNames
-        .filter(name => name.locale === 'ar' || 'fa')
-        .map(name => name.text)
-        .concat(
-          openitiAuthor.otherNames
-            .filter(name => name.locale === 'ar' || 'fa')
-            .flatMap(name => name.texts),
-        );
+      // TODO: track these conflicts to resolve manually
+      // if (linkedAuthors.has(turathAuthor.id)) continue;
+
+      const openitiNames = getOpenitiNames(openitiAuthor);
       const turathName = turathAuthor.name;
 
-      if (
-        openitiNames.some(
-          name =>
-            name === turathName ||
-            normalizeText(name) === normalizeText(turathName) ||
-            arePhoneticallySimilar(name, turathName) ||
-            semanticSimilarity(name, turathName) > 0.5,
-        ) &&
-        !linkedAuthors.has(turathAuthor.id) // TODO: track these conflicts to resolve manually
-      ) {
-        result[openitiAuthor.id] = { id: turathAuthor.id, name: turathAuthor.name };
-        linkedAuthors.add(turathAuthor.id);
-        found = true;
-        break;
+      if (doesNamesMatch(openitiNames, turathName)) {
+        if (linkedAuthors.has(turathAuthor.id)) {
+          conflicts[openitiAuthor.id] = {
+            id: turathAuthor.id,
+            name: turathAuthor.name,
+          };
+        } else {
+          result[openitiAuthor.id] = { id: turathAuthor.id, name: turathAuthor.name };
+          linkedAuthors.add(turathAuthor.id);
+          found = true;
+          break;
+        }
       } else {
         // compare their books
         const openitiBooks = openitiAuthor.books;
@@ -142,95 +211,86 @@ for (const year in deathYearToGroups) {
           turathAuthor.id in turathAuthorIdToBooks
             ? turathAuthorIdToBooks[turathAuthor.id]!
             : [];
-
         for (const openitiBook of openitiBooks) {
           for (const turathBook of turathBooks) {
-            const openitiBookTitle = openitiBook.primaryNames
-              .filter(name => name.locale === 'ar' || 'fa')
-              .map(name => name.text)
-              .concat(
-                openitiBook.otherNames
-                  .filter(name => name.locale === 'ar' || 'fa')
-                  .flatMap(name => name.texts),
-              );
-
+            const openitiBookTitles = getOpenitiNames(openitiBook);
             const turathBookTitle = turathBook.name;
-
-            if (
-              openitiBookTitle.some(
-                name =>
-                  name === turathBookTitle ||
-                  normalizeText(name) === normalizeText(turathBookTitle) ||
-                  arePhoneticallySimilar(name, turathBookTitle) ||
-                  semanticSimilarity(name, turathBookTitle) > 0.5,
-              ) &&
-              !linkedAuthors.has(turathAuthor.id)
-            ) {
-              result[openitiAuthor.id] = { id: turathAuthor.id, name: turathAuthor.name };
-              linkedAuthors.add(turathAuthor.id);
-              found = true;
-              break;
+            if (doesNamesMatch(openitiBookTitles, turathBookTitle)) {
+              if (linkedAuthors.has(turathAuthor.id)) {
+                conflicts[openitiAuthor.id] = {
+                  id: turathAuthor.id,
+                  name: turathAuthor.name,
+                };
+              } else {
+                result[openitiAuthor.id] = {
+                  id: turathAuthor.id,
+                  name: turathAuthor.name,
+                };
+                linkedAuthors.add(turathAuthor.id);
+                found = true;
+                break;
+              }
             }
           }
-
           if (found) break;
         }
       }
     }
-
-    if (found) break;
   }
 
   i++;
 }
 
-const booksResult: Record<string, number> = {};
-const linkedBooks = new Set<number>();
+// const booksResult: Record<string, number> = {};
+// const linkedBooks = new Set<number>();
 
-for (const openitiAuthorId in result) {
-  const turathAuthorId = result[openitiAuthorId]?.id;
-  if (!turathAuthorId) continue;
+// for (const openitiAuthorId in result) {
+//   const turathAuthorId = result[openitiAuthorId]?.id;
+//   if (!turathAuthorId) continue;
 
-  const openitiBooks = openitiAuthorIdToAuthor[openitiAuthorId]!.books;
-  const turathBooks = turathAuthorIdToBooks[turathAuthorId]!;
+//   const openitiBooks = openitiAuthorIdToAuthor[openitiAuthorId]!.books;
+//   const turathBooks = turathAuthorIdToBooks[turathAuthorId]!;
 
-  if (!turathBooks) continue;
+//   if (!turathBooks) continue;
 
-  // compare the books to link them now
-  for (const openitiBook of openitiBooks) {
-    for (const turathBook of turathBooks) {
-      const openitiBookTitle = openitiBook.primaryNames
-        .filter(name => name.locale === 'ar' || 'fa')
-        .map(name => name.text)
-        .concat(
-          openitiBook.otherNames
-            .filter(name => name.locale === 'ar' || 'fa')
-            .flatMap(name => name.texts),
-        );
-      const turathBookTitle = turathBook.name;
+//   // compare the books to link them now
+//   for (const openitiBook of openitiBooks) {
+//     for (const turathBook of turathBooks) {
+//       const openitiBookTitle = openitiBook.primaryNames
+//         .filter(name => name.locale === 'ar' || 'fa')
+//         .map(name => name.text)
+//         .concat(
+//           openitiBook.otherNames
+//             .filter(name => name.locale === 'ar' || 'fa')
+//             .flatMap(name => name.texts),
+//         );
+//       const turathBookTitle = turathBook.name;
 
-      if (
-        openitiBookTitle.some(
-          name =>
-            name === turathBookTitle ||
-            normalizeText(name) === normalizeText(turathBookTitle) ||
-            arePhoneticallySimilar(name, turathBookTitle) ||
-            semanticSimilarity(name, turathBookTitle) > 0.5,
-        ) &&
-        !linkedBooks.has(turathBook.id)
-      ) {
-        booksResult[openitiBook.id] = { id: turathBook.id, name: turathBook.name };
-        linkedBooks.add(turathBook.id);
-      }
-    }
-  }
-}
+//       if (
+//         openitiBookTitle.some(
+//           name =>
+//             name === turathBookTitle ||
+//             normalizeText(name) === normalizeText(turathBookTitle) ||
+//             arePhoneticallySimilar(name, turathBookTitle) ||
+//             semanticSimilarity(name, turathBookTitle) > 0.5,
+//         ) &&
+//         !linkedBooks.has(turathBook.id)
+//       ) {
+//         booksResult[openitiBook.id] = { id: turathBook.id, name: turathBook.name };
+//         linkedBooks.add(turathBook.id);
+//       }
+//     }
+//   }
+// }
 
 fs.writeFileSync(OUTPUT_PATH, JSON.stringify(result, null, 2));
-fs.writeFileSync(OUTPUT_PATH_BOOKS, JSON.stringify(booksResult, null, 2));
+fs.writeFileSync(CONFLICTS_OUTPUT_PATH, JSON.stringify(conflicts, null, 2));
+// fs.writeFileSync(OUTPUT_PATH_BOOKS, JSON.stringify(booksResult, null, 2));
 
 console.log(`Done!`);
-console.log(`Linked ${Object.keys(result).length} / ${openitiAuthors.length} authors.`);
 console.log(
-  `Linked ${Object.keys(booksResult).length} / ${openitiAuthors.reduce((acc, cur) => acc + cur.books.length, 0)} books.`,
+  `Linked ${Object.keys(result).length} / ${openitiAuthors.length} authors. Conflicts: ${Object.keys(conflicts).length}`,
 );
+// console.log(
+//   `Linked ${Object.keys(booksResult).length} / ${openitiAuthors.reduce((acc, cur) => acc + cur.books.length, 0)} books.`,
+// );
